af265a57dfd1229d8357a980a6b0a56a5d424302
Juna Lee <pompory07@naver.com>
2019-06-18 22:44:09 +0900

    고양이 게시판 추가

--- AndroidCatFood/app/src/main/java/com/inha/androidcatfood/FoodSpotInfoActivity.java
+++ AndroidCatFood/app/src/main/java/com/inha/androidcatfood/FoodSpotInfoActivity.java
@@ -1,7 +1,10 @@
 package com.inha.androidcatfood;
 
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class FoodSpotInfoActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class FoodSpotInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_spot_info);
+        String _id = getIntent().getStringExtra("_id");
+        initControls();
     }
+
+    private void initControls() {
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setBackground(new ShapeDrawable(new OvalShape()));
+        imageView.setClipToOutline(true);
+    }
+
 }
